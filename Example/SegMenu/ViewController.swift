1e758689fe0eaa73dd5e685930afb10d71f070cc
Jia Jia <jiafujia123@gmail.com>
2019-06-25 15:59:07 +0900

    Initial commit

---
+++ Example/SegMenu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SegMenu
+//
+//  Created by skykywind on 06/25/2019.
+//  Copyright (c) 2019 skykywind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
